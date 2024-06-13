64ee27418243f14ac19a2564482045b33579e2ed
JUANZEMANATE06 <zemanatejuan35@gmail.com>
2024-06-13 14:22:33 -0500

    cargando ejercicios

---
+++ ejercicio4.php
@@ -0,0 +1,7 @@
+<?php
+$estacion = readline ("Estacion del año en colombia:");
+
+$mensaje = $estacion == ("invierno") ? "Debe usar la siembra en almacigo" : "Se puede realizar la siembra directa";
+
+echo $mensaje;
+?>
